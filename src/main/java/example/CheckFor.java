20a4260a9caeb69cebd91b33d8fed95146af4fdd
Adwiti789 <adwiti1975@gmail.com>
2019-03-12 08:03:14 -0700

    This is hashMap function

--- src/main/java/example/CheckFor.java
+++ src/main/java/example/CheckFor.java
@@ -1,20 +1,23 @@
 package example;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import model.RequestClass;
+import model.ResponseClass;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class CheckFor {
+public class CheckFor implements RequestHandler<RequestClass, ResponseClass> {
 
     public String checkForLengthOfString(String name, Context context) {
         return "Length of the " + name + " = " + name.length();
     }
 
-    public Map<String, String> returnAMap(String key, String value, Context context) {
-        Map<String, String> map = new HashMap<>();
-        map.put(key, value);
-        return map;
+    @Override
+    public ResponseClass handleRequest(RequestClass requestClass, Context context) {
+        Map<String,String> map = new HashMap<>();
+        map.put(requestClass.getFirstName(), requestClass.getLastName());
+        return new ResponseClass(map);
     }
-
 }
---
+++ src/main/java/model/ResponseClass.java
@@ -0,0 +1,16 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Map;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseClass {
+    private Map<String, String> response;
+}
